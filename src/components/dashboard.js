5251268fa26fa66184fcf24b5a529b62852e6143
Bhavika Chauhan <63841109+bhavika15-star@users.noreply.github.com>
2021-10-12 09:38:03 +0530

    Update dashboard.js

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./dashboard.css";
-import areachart from "../images/area-chart-spline.svg"
-import linechart from "../images/512px-SVG-UC-Area.svg.png"
+import areachart from "../images/area-chart-spline.svg";
+import linechart from "../images/512px-SVG-UC-Area.svg.png";
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import ForumIcon from '@mui/icons-material/Forum';
+import PeopleIcon from '@mui/icons-material/People';
 
 function Dashboard() {
   return (
@@ -19,19 +23,25 @@ function Dashboard() {
       </div>
       <div className="div">
         <div className="a">
+          <RemoveRedEyeIcon sx={{ fontSize: 50}}/>
           <p className="number">8,457</p>
           <p>Daily Visits</p>
-        
         </div>
+
         <div className="a">
+          <ShoppingCartIcon sx={{ fontSize: 50}}/>
           <p className="number">52,160</p>
           <p>Sales</p>
         </div>
+        
         <div className="a">
+          <ForumIcon sx={{ fontSize: 50}} />
           <p className="number">15,823</p>  
           <p>Comments</p>
         </div>
+        
         <div className="a">
+          <PeopleIcon sx={{ fontSize: 50}} />
           <p className="number">36,752</p>
           <p>No. of Visits</p>
         </div>
